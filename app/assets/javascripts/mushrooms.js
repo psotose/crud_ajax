141fac95183b74db395cc5ffc30597d74a42e707
patito <p.sotosellanes@hotmail.com>
2020-03-12 22:10:08 -0300

    CRUD AJAX

---
+++ app/assets/javascripts/mushrooms.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+
+  $.ajaxSetup({
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("X-CSRF-Token",
+      $('meta[name="csrf-token"]').attr("content"));
+    },
+  });
+
+  $("#getButton").click(function() {
+    // clear all comments if any
+    $("ul").html("");
+    // fetch comments from the server
+    $.ajax({
+      type: "GET",
+      url: "/comments",
+      dataType: "script"
+    });
+  });
+
+  //Create comment
+  $("[type=submit]").click(function(e) {
+    e.preventDefault();
+
+    if ($("[name=create]").val() == "") {
+      return false;
+    }
+    $.ajax({
+      type: "post",
+      url: "/comments",
+      data: { comment: $("[name=create]").val() },
+      dataType: "script"
+    });
+  });
+
+  //Search comment
+  $("[name=q]").on("keyup", function() {
+    
+    //Data binding
+    // $('h1')[0].innerHTML = $('this').val()
+    if ($(this).val().length > 2) {
+      $.ajax({
+        type: "get",
+        url: "/comments",
+        data: { q: $(this).val() },
+        dataType: "script"
+      });
+    }
+    if ($(this).val().length == 0) {
+      $.ajax({
+        type: "get",
+        url: "/comments",
+        dataType: "script"
+      });
+    }
+  });
+  //Delete a comment
+  $('ul').on('click', '.delete_comment', function(e){
+    e.preventDefault()
+    var id= $(this).closest('li')[0].id.split('_')[1]
+    $.ajax({
+      type: "delete",
+      url: "/comments/" + id,
+      dataType: "script",
+    });
+  })
+  //Edit a comment
+  $('ul').on('click', '.update_comment', function(e){
+    e.preventDefault()
+    var id= $(this).closest('li')[0].id.split('_')[1]
+    $.ajax({
+      type: "get",
+      url: "/comments/"+id+"/edit",
+      dataType: "script",
+    });
+  })
+  //Update a comment
+  $('#update').on('click', 'button', function(e){
+    e.preventDefault()
+    var content = $(this).closest('form')[0][0].value
+    var id =$(this).closest('form')[0][1].value 
+    
+    $.ajax({
+      type: "patch",
+      url: "/comments/" + id,
+      data: { comment: content },
+      dataType: "script",
+    });
+  })
+  //Show comment
+  $('ul').on('click', 'show_comment', function(e){
+    e.preventDefault()
+    var id= $(this).closest('li')[0].id.split('_')[1]
+    $.ajax({
+      type: "get",
+      url: "/comments/"+id,
+      dataType: "script",
+    });
+  })
+});
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,42 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:destroy, :edit, :update, :show]
+
+  def index
+    @comments = unless params[:q]
+                  Comment.all
+                else      
+                  Comment.where('content like ?', "%#{params[:q]}%") 
+                end  
+    
+  end
+
+  def create
+    @comment = Comment.new(content: comment_params)
+    @comment.save
+  end
+
+  def edit
+  end  
+
+  def show
+  end  
+
+  def update
+    @comment.content = comment_params
+    @comment.save
+  end  
+
+  def destroy
+      @comment.destroy
+  end  
+  
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end  
+
+  def comment_params
+    params.require(:comment)
+  end  
+end
